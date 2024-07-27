7b9a05e43e82bb059840fc5c708e82f9f3f94947
Abdo-Hammad <abdohammad2030@gmail.com>
2024-07-27 15:12:11 +0300

    Add files via upload

---
+++ JS/jquery.js
@@ -0,0 +1,20 @@
+// Start input2
+$(document).ready(function()
+{
+ $("#p").click(function(){
+   $(".span").slideToggle();
+    
+ });
+$(".p1").click(function(){
+   $(".span1").slideToggle();
+ });
+
+$(".p2").click(function(){
+   $(".span2").slideToggle();
+ });
+
+	$(".p3").click(function(){
+   $(".span3").slideToggle();
+ }); 
+});
+ // End input2
